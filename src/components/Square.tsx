cd35cbcf93bfb8068757b54c6dfc1be4106a9121
Marcos <mdiannibelli@gmail.com>
2024-06-01 15:32:49 -0300

    fixing colors

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -10,7 +10,7 @@ export const Square = ({square, cellClicked, keyToCheck, gameStatus, handleClick
     const colIndex  = keyToCheck.split("-")[0]
     const celIndex = keyToCheck.split('-')[1]
   return (
-    <div className={`border-2 p-2 bg-gray-800 border-white w-20 h-20 flex justify-center items-center text-2xl cursor-pointer text-white ${cellClicked.includes(keyToCheck) && square === 'B' && 'bg-red-500'} ${cellClicked.includes(keyToCheck) && square !== 'B'  && 'bg-gray-400'}`}>
+    <div className={`border-2 p-2 bg-gray-800 border-white w-20 h-20 flex justify-center items-center text-2xl cursor-pointer text-white ${cellClicked.includes(keyToCheck) && square === 'B' && 'bg-red-500'} ${cellClicked.includes(keyToCheck) && square !== 'B'  && 'bg-green-500'}`}>
         {/* {square === 'B' ? '💣' : '💎'} */}
         {
             cellClicked.includes(keyToCheck) ? <span>{square === 'B' ? '💣' : square}</span> : <button disabled={gameStatus === 'won' || gameStatus === 'lost'} onClick={() => handleClick(colIndex, celIndex)} className={`${gameStatus === 'won' || gameStatus === 'lost' && 'bg-black'} cursor-pointer h-full w-full`}></button>
