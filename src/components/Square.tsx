fb3843b63878899aff82560c6a7d6388ac737c29
Marcos <mdiannibelli@gmail.com>
2024-06-01 15:30:10 -0300

    fix nums

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -10,10 +10,10 @@ export const Square = ({square, cellClicked, keyToCheck, gameStatus, handleClick
     const colIndex  = keyToCheck.split("-")[0]
     const celIndex = keyToCheck.split('-')[1]
   return (
-    <div className={`border-2 p-2 bg-gray-800 border-white w-20 h-20 flex justify-center items-center text-2xl cursor-pointer text-white ${cellClicked.includes(keyToCheck) && 'bg-gray-700'} ${cellClicked.includes(keyToCheck) && square === 'B' && 'bg-red-500'} ${cellClicked.includes(keyToCheck) && square !== 'B' && 'bg-gray-500'}`}>
-        {/* {square === 'B' ? '💣' : square} */}
+    <div className={`border-2 p-2 bg-gray-800 border-white w-20 h-20 flex justify-center items-center text-2xl cursor-pointer text-white ${cellClicked.includes(keyToCheck) && square === 'B' && 'bg-red-500'} ${cellClicked.includes(keyToCheck) && square !== 'B'  && 'bg-gray-400'}`}>
+        {/* {square === 'B' ? '💣' : '💎'} */}
         {
-            cellClicked.includes(keyToCheck) ? <span>{square === 'B' ? '💣' : '💎'}</span> : <button disabled={gameStatus === 'won' || gameStatus === 'lost'} onClick={() => handleClick(colIndex, celIndex)} className={`${gameStatus === 'won' || gameStatus === 'lost' && 'bg-black'} cursor-pointer h-full w-full`}></button>
+            cellClicked.includes(keyToCheck) ? <span>{square === 'B' ? '💣' : square}</span> : <button disabled={gameStatus === 'won' || gameStatus === 'lost'} onClick={() => handleClick(colIndex, celIndex)} className={`${gameStatus === 'won' || gameStatus === 'lost' && 'bg-black'} cursor-pointer h-full w-full`}></button>
         }
     </div>
   )
